a771eea3602c5bc2c6ce237f20f9e3d78f84438b
20160911@student.hust.edu.vn <20160911@student.hust.edu.vn>
2018-03-22 22:47:10 +0700

    ...

--- TIM_KIEM/main.cpp
+++ TIM_KIEM/main.cpp
@@ -11,11 +11,11 @@ void InPut();
 int nhap_key();
 int tim_kiem_tuan_tu(int key);
 int tim_kiem_nhi_phan1(int key); ///cai dat voi vong lap
-int tim_kiem_nhi_phan2(int key, int l, int r); ///cai dat voi de quy
+//int tim_kiem_nhi_phan2(int key, int l, int r); ///cai dat voi de quy
 
 int main()
 {
-    clockid_t start;
+    clockid_t start, finish;
     string f1, f2;
 
     f1 = "input.txt";
@@ -29,17 +29,17 @@ int main()
     InPut();
     nhap_key();
 
-    start = clock();
+    start = finish = clock();
     cout << tim_kiem_tuan_tu(key);
-    cout << clock() - start << endl;
+    cout << finish - start << endl;
 
-    start = clock();
+    start = finish = clock();
     cout << tim_kiem_nhi_phan1(key);
-    cout << clock() - start << endl;
+    cout << finish - start << endl;
 
-    start = clock();
-    cout << tim_kiem_nhi_phan2(key, 0, n - 1);
-    cout << clock() - start << endl;
+//    start = finish = clock();
+//    cout << tim_kiem_nhi_phan2(key, 0, n - 1);
+//    cout << clock() - start << endl;
 
     fileInPut.close();
     return 0;
@@ -82,14 +82,14 @@ int tim_kiem_nhi_phan1(int key)
     return -1;
 }
 
-int tim_kiem_nhi_phan2(int key, int l, int r)
-{
-    int m = (l + r) / 2;
-    if (keys[m] == key)
-        return m;
-    else if (keys[m] < key)
-        return tim_kiem_nhi_phan2(key, l, m-1);
-    else
-        return tim_kiem_nhi_phan2(key, m + 1, r);
-    return -1;
-}
+//int tim_kiem_nhi_phan2(int key, int l, int r)
+//{
+//    int m = (l + r) / 2;
+//    if (keys[m] == key)
+//        return m;
+//    else if (keys[m] < key)
+//        return tim_kiem_nhi_phan2(key, l, m-1);
+//    else
+//        return tim_kiem_nhi_phan2(key, m + 1, r);
+//    return -1;
+//}
