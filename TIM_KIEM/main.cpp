984c2bcd77e261d32005c63b632bb16e2d9dcab1
20160911@student.hust.edu.vn <20160911@student.hust.edu.vn>
2018-03-22 19:40:54 +0700

    file tim_kiem

---
+++ TIM_KIEM/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int key;
+
+int nhap_key();
+int tim_kiem_tuan_tu(int key);
+int tim_kiem_nhi_phan1(int key); ///cai dat voi vong lap
+int tim_kiem_nhi_phan2(int key); ///cai dat voi de quy
+
+int main()
+{
+    clockid_t start, finish;
+    fstream fileInPut;
+    string f1, f2;
+    f1 = "input.txt";
+    f2 = "input1.txt";
+    fileInPut.open(&f1, ios::out);
+    if(fileInPut.fail()){
+        cout << "cannot open this file" << endl;
+    }
+    nhap_key();
+    tim_kiem_tuan_tu(key);
+
+    fileInPut.close();
+    return 0;
+}
